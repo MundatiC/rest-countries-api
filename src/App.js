dc11e1772606d51729143402c052b43fd83147c9
MundatiC <calebngethe230@gmail.com>
2023-05-30 10:01:41 +0300

    fixed search and some design issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Filter from "./components/Filter";
 import Countries from "./components/Countries";
 import Country from "./components/Country";
 
