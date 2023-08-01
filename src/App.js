424d6336dfaf797f62b56dfe3baf9dee72fe5e18
MundatiC <calebngethe230@gmail.com>
2023-08-01 12:00:56 +0300

    using HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Countries from "./components/Countries";
 import Country from "./components/Country";
